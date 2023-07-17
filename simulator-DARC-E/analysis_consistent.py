a9fad4c93bca0c5ac43caefd2f5944b81b438b92
yqy1998 <3475354147@qq.com>
2023-07-17 12:00:57 +0800

    上传单个服务器的shinjuku版本

--- simulator-DARC-E/analysis_consistent.py
+++ simulator-DARC-E/analysis_consistent.py
@@ -5,7 +5,7 @@ import os
 import json
 import datetime
 
-RESULTS_DIR_NAME = "/home/yqy/simulator-DARC-E/results/"
+RESULTS_DIR_NAME = "/home/yqy/simulation_schedule/simulator-DARC-E/results/"
 TASK_FILE_NAME = "task_times.csv"
 
 def get_job_complation_time(result_folder):
@@ -15,7 +15,7 @@ def get_job_complation_time(result_folder):
     for entry in os.scandir(dir_path):
         if entry.is_dir():
             count += 1
-    job_info = [[0, 0, 0, 0, 0] for _ in range(10000)]
+    job_info = [[0, 0, 0, 0, 0, 0] for _ in range(10000)]
     i = 0
     data_num = 0
     job_num= 0
@@ -29,13 +29,15 @@ def get_job_complation_time(result_folder):
             data = line.strip().split(",")
             job_id = int(data[10])
             if job_info[job_id][4] == 0 and job_info[job_id][3] == 0 and job_info[job_id][2] == 0 and job_info[job_id][1] == 0:
-                job_info[job_id] = [job_id, int(data[11]), int(data[12]), int(data[2]), int(data[13])]
+                job_info[job_id] = [job_id, int(data[11]), int(data[12]), int(data[2]), int(data[13]),0]
                 job_num += 1
+                job_info[job_id][5] = job_info[job_id][5] + 1
                 # print(job_id, int(data[11]), int(data[12]), int(data[2]), int(data[13]))
             else:
                 job_info[job_id][3] += int(data[2])
                 if job_info[job_id][4] < int(data[13]):
                     job_info[job_id][4] = int(data[13])
+                job_info[job_id][5] = job_info[job_id][5] + 1
 
         task_file.close()
         i = i+1
@@ -49,8 +51,8 @@ def get_job_complation_time(result_folder):
 
 if __name__ == "__main__":
 
-    result_ordered_folder = "sim_cluster_simulation_23-07-11_11:49:24_reuse_cluster_simulation_23-07-11_11:35:52_tasks_info__ordered_cluster_random/"
-    result_folder = "sim_cluster_simulation_23-07-11_11:44:13_reuse_cluster_simulation_23-07-11_11:35:52_tasks_info__cluster_random/"
+    result_ordered_folder = "sim_cluster_simulation_23-07-12_17:11:40_reuse_cluster_simulation_23-07-12_10:58:55_tasks_info__ordered_cluster_random/"
+    result_folder = "sim_cluster_simulation_23-07-12_14:41:19_reuse_cluster_simulation_23-07-12_10:58:55_tasks_info__cluster_random/"
 
     job_time = get_job_complation_time(result_folder)
     job_time_ordered = get_job_complation_time(result_ordered_folder)
@@ -65,16 +67,16 @@ if __name__ == "__main__":
         substring = result_folder[start_index + len(search_str):]
         str1 = search_str + substring
         new_str = str1[:-1]
-    analysis_result_path = "/home/yqy/simulator-DARC-E/analysis_consistent_result/"+ datetime.datetime.now().strftime("%y-%m-%d_%H:%M:%S")+new_str+".csv"
+    analysis_result_path = "/home/yqy/simulation_schedule/simulator-DARC-E/analysis_consistent_result/"+ datetime.datetime.now().strftime("%y-%m-%d_%H:%M:%S")+new_str+".csv"
     job_file = open(analysis_result_path,"w")
     # Write task information（需要修改只写入成功完成的任务）
-    header = ["job_id","tasks_num_of_single_job","task_generation_time","total_time","complation_time","complation_time_ordered","shortened_completion_time"
-              "job_id_order","tasks_num_of_single_job_order","task_generation_time_order","total_time_order"]
+    header = ["job_id","tasks_num_of_single_job","task_generation_time","total_time","complation_time","finish_num","shortened_completion_time"
+              "job_id_order","tasks_num_of_single_job_order","task_generation_time_order","total_time_order","complation_time_ordered","finish_num"]
     job_file.write(','.join(header) + "\n")
     i = 0
-    while i<10000: 
-        stats = [job_time[i][0],job_time[i][1],job_time[i][2],job_time[i][3],job_time[i][4],
-                job_time_ordered[i][4],job_time[i][4]-job_time_ordered[i][4],job_time_ordered[i][0],job_time_ordered[i][1],job_time_ordered[i][2],job_time_ordered[i][3],]
+    while i<10000 and job_time[i][5] == job_time[i][1] == job_time_ordered[i][1] : 
+        stats = [job_time[i][0],job_time[i][1],job_time[i][2],job_time[i][3],job_time[i][4],job_time[i][5],
+                job_time[i][4]-job_time_ordered[i][4],job_time_ordered[i][0],job_time_ordered[i][1],job_time_ordered[i][2],job_time_ordered[i][3],job_time_ordered[i][4],job_time_ordered[i][5]]
         stats = [str(x) for x in stats]
         job_file.write(','.join(stats) + "\n")
         i = i+1
